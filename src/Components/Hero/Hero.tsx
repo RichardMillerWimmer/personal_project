c0a85ee6a80ec02ec6a9482de8409ef014b89772
Richard Miller <richardmmill@gmail.com>
2022-04-06 19:13:16 -0400

    add types.d.ts and Hero to ts

---
+++ src/Components/Hero/Hero.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Demo from '../../images/DemoReel.mp4';
+
+function Hero() {
+
+    return (
+        <div className='hero'>
+            <div className='heroText'>
+                <h1>POLYMATH<br></br> VISUALS</h1>
+                <h3>Welcome to our texture packages store</h3>
+            </div>
+            <video className='videoTag'
+                autoPlay
+                loop
+                muted
+            >
+                <source src={Demo} type='video/mp4' />
+            </video>
+        </div>
+    );
+};
+
+export default Hero;
