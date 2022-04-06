754f7858e31d4f3cffd685ef22c2336538a4e1b9
Richard Miller <richardmmill@gmail.com>
2022-04-06 19:54:22 -0400

    start of authReducer to ts

---
+++ authReducer.ts
@@ -0,0 +1,50 @@
+const initialState = {
+    firstName: '',
+    userId: null,
+    admin: null
+};
+
+type User = {
+    firstName: string;
+    userId: number;
+    admin: boolean;
+}
+
+type AuthAction = {
+    type: 'UPDATE_USER' | 'LOGOUT';
+    payload: User
+}
+
+const UPDATE_USER = 'UPDATE_USER';
+const LOGOUT = 'LOGOUT';
+
+export function updateUser(user: User) {
+    // console.log(user)
+    return {
+        type: UPDATE_USER,
+        payload: user
+    }
+};
+
+export function logoutUser() {
+    return {
+        type: LOGOUT
+    }
+};
+
+
+export default function authReducer(state = initialState, action: AuthAction) {
+    console.log(action)
+    switch (action.type) {
+        case UPDATE_USER:
+            return {
+                ...state,
+                firstName: action.payload.firstName,
+                userId: action.payload.userId,
+                admin: action.payload.admin
+            };
+        case LOGOUT:
+            return initialState;
+        default: return state
+    }
+};
