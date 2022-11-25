a8c9ce37fd260c564918a1521af8f437019c9f8d
Richard Miller <richardmmill@gmail.com>
2022-11-25 08:34:42 -0500

    clean up Auth with new Input

--- src/Components/Auth/Auth.tsx
+++ src/Components/Auth/Auth.tsx
@@ -14,12 +14,10 @@ function Auth(props: { updateUser: (arg0: { firstName: any; userId: any; admin?:
     const [loginEmail, setLoginEmail] = useState<string>('');
     const [loginPassword, setLoginPassword] = useState<string>('');
 
-
     function register(): void {
         axios.post('/api/auth/register', { email, firstName, lastName, password })
             .then(res => {
                 const { firstName, userId } = res.data
-
                 props.updateUser({ firstName, userId })
                 props.history.push('/')
             })
@@ -27,9 +25,6 @@ function Auth(props: { updateUser: (arg0: { firstName: any; userId: any; admin?:
     };
 
     function login(): void {
-        // console.log('hit login')
-        // console.log(loginEmail)
-        // console.log(loginPassword)
         axios.post('/api/auth/login', { loginEmail, loginPassword })
             .then(res => {
                 console.log(res.data)
@@ -41,71 +36,25 @@ function Auth(props: { updateUser: (arg0: { firstName: any; userId: any; admin?:
             .catch(err => console.log(err, err.message))
     };
 
-    // const registerEnterPress = (event: KeyboardEvent<HTMLInputElement>): void => {
-    //     if (event.key === '13') {
-    //         register()
-    //     }
-    // };
-    // const loginEnterPress = (event: KeyboardEvent<HTMLInputElement>): void => {
-    //     if (event.key === '13') {
-    //         login()
-    //     }
-    // };
-
     return (
         <div className="authContainer">
             <form className='register box'>
                 <h2>Register</h2>
                 <Input value={email} label='email' handler={setEmail} />
-                {/* <label htmlFor='email'>email:</label>
-                <input
-                    id='email'
-                    onChange={event => setEmail(event.target.value)}
-                    value={email} /> */}
                 <br></br>
                 <Input value={firstName} label='firstName' handler={setFirstName} />
-                {/* <label htmlFor='firstName'>first name:</label>
-                <input
-                    id='firstName'
-                    onChange={event => setFirstName(event.target.value)}
-                    value={firstName} /> */}
                 <br></br>
                 <Input value={lastName} label='lastName' handler={setLastName} />
-                {/* <label htmlFor='lastName'>last name:</label>
-                <input
-                    id='lastName'
-                    onChange={event => setLastName(event.target.value)}
-                    value={lastName} /> */}
                 <br></br>
                 <Input value={password} label='password' handler={setPassword} type='password' />
-                {/* <label htmlFor='password'>password:</label>
-                <input
-                    id='password'
-                    type='password'
-                    onChange={event => setPassword(event.target.value)}
-                    value={password}
-                    onKeyPress={registerEnterPress} /> */}
                 <br></br>
                 <Button onClick={register} type='submit'>register</Button>
             </form>
             <form className='login box'>
                 <h2>Login</h2>
                 <Input value={loginEmail} label='loginEmail' text='email' handler={setLoginEmail} />
-                {/* <label htmlFor='loginEmail'>email:</label>
-                <input
-                    id='loginEmail'
-                    onChange={event => setLoginEmail(event.target.value)}
-                    value={loginEmail}
-                /> */}
                 <br></br>
                 <Input value={loginPassword} label={'loginPassword'} text='password' handler={setLoginPassword} type='password' />
-                {/* <label htmlFor='loginPassword'>password:</label>
-                <input
-                    id='loginPassword'
-                    type='password'
-                    onChange={event => setLoginPassword(event.target.value)}
-                    value={loginPassword}
-                    onKeyPress={loginEnterPress} /> */}
                 <br></br>
                 <Button onClick={login} type='submit'>login</Button>
             </form>
